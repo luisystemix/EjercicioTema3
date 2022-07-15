ac8cd3ffdcdb097c5a52172b1b44ae1f0bd989f9
JOSE LUIS ARUQUIPA HILARI <luisystem@gmail.com>
2022-07-15 08:46:30 -0400

    Update Main.java
    Ejercicio Tema 3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("[ Ejercicio Tema 3 ]");
+        System.out.println("==Parte 1==");
+        System.out.println("La suma de 3 variables es:"+suma(5,9,12));
+        System.out.println("==Parte 2==");
+        Coche miCoche = new Coche();
+        System.out.println("Se agrega Puerta");
+        miCoche.agregarPuerta();
+        System.out.println(miCoche.puerta);
+        System.out.println("Se quita Puerta");
+        miCoche.quitarPuerta();
+        System.out.println(miCoche.puerta);
+    }
+    public static int suma(int a,int b, int c){
+        return a+b+c;
+    }
+    static class Coche{
+        public int puerta = 4;
+        public void agregarPuerta(){
+            this.puerta++;
+        }
+        public void quitarPuerta(){
+            this.puerta--;
+        }
+
     }
 }
